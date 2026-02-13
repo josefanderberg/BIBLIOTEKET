c787ad82e66f0d29020f34d9adc86e343bee915c
josefanderberg <josef.anderberg@gmail.com>
2026-02-13 19:43:33 +0100

    Update Book class

--- BIBLIOTEK/Book.cs
+++ BIBLIOTEK/Book.cs
@@ -1,8 +1,26 @@
 public class Book
 {
-    public string ISBN { get; set; }
+    public string ISBN { get; }
     public string Title { get; set; }
     public string Author { get; set; }
     public int PublishedYear { get; set; }
     public bool IsAvailable { get; set; }    
-}
+
+    public Book(string isbn, string title, string author, int publishedYear)
+    {
+        ISBN = isbn;
+        Title = title;
+        Author = author;
+        PublishedYear = publishedYear;
+        IsAvailable = true;
+    }
+
+    public void GetInfo()           
+    {
+        Console.WriteLine($"ISBN: {ISBN}");
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Published Year: {PublishedYear}");
+        Console.WriteLine($"Is Available: {IsAvailable}");
+    }
+}   
