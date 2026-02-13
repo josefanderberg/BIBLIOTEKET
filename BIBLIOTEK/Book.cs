4ae0d3cc5007fa266e5ecfd484ddb69d9ec461f7
josefanderberg <josef.anderberg@gmail.com>
2026-02-13 19:42:33 +0100

    Add Book class with constructor and GetInfo method

---
+++ BIBLIOTEK/Book.cs
@@ -0,0 +1,8 @@
+public class Book
+{
+    public string ISBN { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int PublishedYear { get; set; }
+    public bool IsAvailable { get; set; }    
+}
