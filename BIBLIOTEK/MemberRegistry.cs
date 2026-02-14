a5eeb1e1dbaf879af816a8665f1fd73ac34224cb
josefanderberg <josef.anderberg@gmail.com>
2026-02-14 16:39:06 +0100

    Add code for MemberRegistr

--- BIBLIOTEK/MemberRegistry.cs
+++ BIBLIOTEK/MemberRegistry.cs
@@ -1,4 +1,25 @@
 public class MemberRegistry
 {
+    private List<Member> members = new List<Member>();
+
+    public void AddMember(Member member)
+    {
+        members.Add(member);
+    }
+
+    public Member GetMemberById(string memberId)
+    {
+        return members.FirstOrDefault(m => m.MemberId == memberId);
+    }
+
+    public List<Member> GetAllMembers()
+    {
+        return new List<Member>(members);
+    }
+
+    public int GetTotalMemberCount()
+    {
+        return members.Count;
+    }   
 
 }
