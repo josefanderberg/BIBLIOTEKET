a3b08bdb6afabbcd4c90ce47381dcb79ab998970
josefanderberg <josef.anderberg@gmail.com>
2026-02-14 16:43:31 +0100

    Add LoadManager

--- BIBLIOTEK/LoanManager.cs
+++ BIBLIOTEK/LoanManager.cs
@@ -1,4 +1,25 @@
 public class LoanManager
 {
+    private List<Loan> loans = new List<Loan>();
+
+    public void AddLoan(Loan loan)
+    {
+        loans.Add(loan);
+    }
+
+    public Loan GetLoanByLoanId(string loanId)
+    {
+        return loans.FirstOrDefault(l => l.LoanId == loanId);
+    }
+
+    public List<Loan> GetAllLoans()
+    {
+        return new List<Loan>(loans);
+    }
+
+    public int GetTotalLoanCount()
+    {
+        return loans.Count;
+    }   
 
 }
